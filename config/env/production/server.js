1118f8062139374401abbc431d82b546650f3bf5
mhbaando <maxamuudyare21@gmail.com>
2023-03-30 20:51:52 +0300

    configured for production

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   proxy: true,
-  url: env("MY_HEROKU_URL"),
+  url: env('APP_URL'), // Sets the public URL of the application.
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array('APP_KEYS')
   },
 });
